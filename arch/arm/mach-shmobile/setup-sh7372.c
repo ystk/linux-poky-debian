28829f5a27b7d8d71a832142129c9fe1636bc7a8
Shimoda, Yoshihiro <yoshihiro.shimoda.uh@renesas.com>
2012-01-10 14:21:31 +0900

    arm: mach-shmobile: add a resource name for shdma
    Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>
    (cherry picked from commit 20052462948795914011f2fea0e77767d55cb48b)
    
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- arch/arm/mach-shmobile/setup-sh7372.c
+++ arch/arm/mach-shmobile/setup-sh7372.c
@@ -505,7 +505,7 @@ static struct resource sh7372_dmae0_resources[] = {
 		.flags	= IORESOURCE_MEM,
 	},
 	{
-		/* DMA error IRQ */
+		.name	= "error_irq",
 		.start	= evt2irq(0x20c0),
 		.end	= evt2irq(0x20c0),
 		.flags	= IORESOURCE_IRQ,
@@ -533,7 +533,7 @@ static struct resource sh7372_dmae1_resources[] = {
 		.flags	= IORESOURCE_MEM,
 	},
 	{
-		/* DMA error IRQ */
+		.name	= "error_irq",
 		.start	= evt2irq(0x21c0),
 		.end	= evt2irq(0x21c0),
 		.flags	= IORESOURCE_IRQ,
@@ -561,7 +561,7 @@ static struct resource sh7372_dmae2_resources[] = {
 		.flags	= IORESOURCE_MEM,
 	},
 	{
-		/* DMA error IRQ */
+		.name	= "error_irq",
 		.start	= evt2irq(0x22c0),
 		.end	= evt2irq(0x22c0),
 		.flags	= IORESOURCE_IRQ,
